308c87b4b3b0bb56765df1256f99aa167c6c14c8
jampibryan <jampibryan@gmail.com>
2023-03-27 00:59:27 -0500

    script api finder v1

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 
 // VARIABLES
 const lista = document.querySelector('.lista')
+const btn_search = document.querySelector('.btn_search')
+const nameU = document.querySelector('#nameU')
+
 
 // FUNCTIONS
+const finder = async () => {
 
-const finding = async () => {
     const url = 'http://universities.hipolabs.com/search?name=middle'
     const respone = await fetch(url)
     const data = await respone.json();
 
+    const nameUni = nameU.value
     let resultados = ''
-    for (let i = 0; i < data.length; i++) {
-        resultados += `<li> <a href="${data[i].web_pages}"> ${data[i].name} </a> </li>`
-    }
 
+    for (let i = 0; i < data.length; i++) {
+        let name = data[i].name
+
+        // let regex = new RegExp("^" + nameUni + ".", "gi");
+        let regex = new RegExp(nameUni + ".", "gi");
+        let result = regex.test(name)
+
+        if (result) {
+            resultados += `<li> <a href="${data[i].web_pages}"> ${data[i].name} </a> </li>`
+        }
+    }
     lista.innerHTML = resultados
 }
 
-finding();
-
-
-
+btn_search.addEventListener('click', finder)
