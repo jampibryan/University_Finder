00bf35f8f23a10cdbb298aa4b5e963c443fd6a91
jampibryan <jampibryan@gmail.com>
2023-03-27 18:14:13 -0500

    script api finder v2

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const lista = document.querySelector('.lista')
 const btn_search = document.querySelector('.btn_search')
 const nameU = document.querySelector('#nameU')
+const caption = document.querySelector('.caption')
 
 
 // FUNCTIONS
@@ -14,16 +15,25 @@ const finder = async () => {
 
     const nameUni = nameU.value
     let resultados = ''
+    let captionText = ''
 
+    let countResultados = 0;
     for (let i = 0; i < data.length; i++) {
         let name = data[i].name
 
-        // let regex = new RegExp("^" + nameUni + ".", "gi");
-        let regex = new RegExp(nameUni + ".", "gi");
+        let regex = new RegExp(nameUni, "gi");
         let result = regex.test(name)
 
         if (result) {
             resultados += `<li> <a href="${data[i].web_pages}"> ${data[i].name} </a> </li>`
+            countResultados++
+        }
+        if (countResultados == 0) {
+            captionText = `No se encontró ningún resultado`
+            caption.innerHTML = captionText
+        } else {
+            captionText = `Resultados`
+            caption.innerHTML = captionText
         }
     }
     lista.innerHTML = resultados
